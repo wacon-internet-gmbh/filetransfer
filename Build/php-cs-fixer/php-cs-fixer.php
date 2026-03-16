81513acbed4b1e610f363732c616606fb8d63a23
slavlee <info@slavlee.de>
2026-03-16 07:00:57 +0100

    - [TASK] Increase timeout on parallel config for php-cs-fixer

--- Build/php-cs-fixer/php-cs-fixer.php
+++ Build/php-cs-fixer/php-cs-fixer.php
@@ -1,5 +1,7 @@
 <?php
 
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config->setHeader(
     'This file is part of the "filetransfer" Extension for TYPO3 CMS.
@@ -74,4 +76,5 @@ $config->setFinder(
         'whitespace_after_comma_in_array' => ['ensure_single_space' => true],
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     ]);
+$config->setParallelConfig(ParallelConfigFactory::detect()->withTimeout(300));
 return $config;
